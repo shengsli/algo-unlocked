20dd998dce955ae08484777ec04e939a6150cfa2
shengsli <sheng.s.li@outlook.com>
2019-08-26 15:26:52 +0100

    add .gitignore; update linearSearch.cpp

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -1,13 +1,15 @@
 #include <cstdio>
 
-int linearSearch(int[] arr, int numOfEls, int val) {
+int linearSearch(int arr[], int numOfEls, int val) {
 	for (int i=0; i<numOfEls; ++i) {
 		if (arr[i]==val) return i;
 	}
-	return NULL;
+	return -1;
 }
 
-int main (int argc, char[] *argv) {
-	int[] arr = {1,2,3,4,5};
-	linearSearch(arr,5,3);
+int main (int argc, char *argv[]) {
+	int val = 3;
+	int arr[5] = {1,2,3,4,5};
+	int idx = linearSearch(arr,5,val);
+	printf("find %d at %d\n", val, idx);
 }
